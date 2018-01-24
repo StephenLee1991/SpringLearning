af34187c5b06ed41f03dc309bf88c7a8dd6c1525
StephenLee <lixiaoxiao@casicloud.cn>
2018-01-24 13:02:59 +0800

    firstspring

--- firstspring/src/main/java/basic/MessageService.java
+++ firstspring/src/main/java/basic/MessageService.java
@@ -10,11 +10,11 @@ public class MessageService {
         System.out.println("通过Spring创建了MessageService对象！");
     }
 
-    //初始化方法
+    //初始化方法，容器创建好bean的实例之后，会立即调用初始化方法
     public void init(){
         System.out.println("MessageService的初始化方法！");
     }
-    //销毁方法
+    //销毁方法，容器关闭之前会调用销毁方法
     public void destroy(){
         System.out.println("MessageService的销毁方法！");
     }
